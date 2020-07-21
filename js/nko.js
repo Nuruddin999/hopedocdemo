a9babc0827cdd4824393c049bbf4189d2873f2e4
Nuruddin <sg7720@gmail.com>
2020-07-22 00:14:50 +0300

    nko mobile

--- js/nko.js
+++ js/nko.js
@@ -1,4 +1,19 @@
 'use strict'
-import helpboard from "./helpboard"
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    watchOverflow: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
 ReactDOM.render(<QAs />, document.getElementById('answers'))
 ReactDOM.render(helpboard, document.getElementById('board'))
