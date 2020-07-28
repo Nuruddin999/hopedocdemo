79bda6e101e09b8ca4180f111f026c513b23fd86
Nuruddin <sg7720@gmail.com>
2020-07-28 18:11:07 +0300

    actions in main page and nko page

---
+++ js/index.js
@@ -0,0 +1,124 @@
+var mySwiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+            el: '.swiper-pagination',
+      }
+})
+var btn = document.querySelector(".hamburger")
+btn.addEventListener('click', () => {
+      document.body.addEventListener('touchstart', function (e) { e.preventDefault(); });
+      document.body.style.overflow = 'hidden';
+      document.querySelector("body > div.navdrawr").style.transform = "translateX(0)";
+})
+
+$('.navdrawheader, img').click(function () {
+      $('html, body').css('overflowY', 'auto');
+      $('.navdrawr').css('transform', 'translateX(-100%)');
+});
+var summ = 0;
+$('.item').click(function () {
+      $('.item').css({ "background": "white" });
+      $('.item').find("span").css({ color: "#0b1635" })
+      var t = $(this).text();
+      summ = t;
+      console.log(summ)
+      $(this).css({ "background": "#367bf5" });
+      $(this).find("span").css({ color: "white" })
+});
+let buttonPressed = true;
+let timeIsChecked = false;
+let monthisChecked = true;
+$('#monthly').click(function name(params) {
+      if (monthisChecked) {
+            return
+      }
+      monthisChecked = true;
+      if (monthisChecked) {
+            $('#monthly').css({
+                  background: "#367bf5",
+                  color: "white"
+            })
+            $('#onetime').css({
+                  background: "white",
+                  color: "#367bf5"
+            })
+
+      } else {
+            $('#monthly').css({
+                  background: "white",
+                  color: "#367bf5"
+            })
+            $('#onetime').css({
+                  background: "#367bf5",
+                  color: "white"
+            })
+      }
+
+      timeIsChecked = false
+})
+$('#onetime').click(function name(params) {
+      if (timeIsChecked) {
+            return
+      }
+      timeIsChecked = true
+      if (timeIsChecked) {
+            $('#onetime').css({
+                  background: "#367bf5",
+                  color: "white"
+            })
+            $('#monthly').css({
+                  background: "white",
+                  color: "#367bf5"
+            })
+      } else {
+            $('#onetime').css({
+                  background: "red",
+                  color: "white"
+            })
+            $('#monthly').css({
+                  background: "#367bf5",
+                  color: "white"
+            })
+      }
+      monthisChecked = false
+})
+let name = document.getElementById("name")
+let phone = document.getElementById("phone")
+let email = document.getElementById("eml")
+let agree = document.getElementById('agree')
+let button = document.getElementById("send-button")
+let warning = document.getElementById("warning")
+const getConsultation = (event) => {
+      event.preventDefault();
+      if (name.value === "" || phone.value === "" || email.value === "") return
+      if (!agree.checked) {
+            warning.style.display = "block"
+            setTimeout(() => { warning.style.display = "none"; }, 1000)
+            return
+      }
+      let data = {
+            name, phone, eml,
+            from: "main"
+      }
+      console.log(data)
+      axios.post('sendForm.php', {
+            name: name.value,
+            phone: phone.value,
+            email: email.value,
+            from: "main"
+
+      })
+            .then(function (response) {
+                  console.log(response);
+            })
+            .catch(function (error) {
+                  console.log(error);
+            });
+
+}
+let form = document.getElementById("myForm");
+form.addEventListener('submit', getConsultation);
--- js/nko.js
+++ js/nko.js
@@ -23,6 +23,44 @@ var mySwiper = new Swiper('.swiper-container', {
         el: '.swiper-scrollbar',
     },
 })
+let fioname = document.getElementById("fioform")
+let nkoname = document.getElementById("nko-name-form")
+let phone = document.getElementById("phone-form")
+let email = document.getElementById("email-form")
+let agree = document.getElementById('agr-check')
+let button = document.getElementById("send-button")
+let warning = document.getElementById("warning")
+const getConsultation = (event) => {
+    event.preventDefault();
+    if (fioname.value === "" || phone.value === "" || email.value === "") return
+    if (!agree.checked) {
+        warning.style.display = "block"
+        setTimeout(() => { warning.style.display = "none"; }, 1000)
+        return
+    }
+    let data = {
+        name, phone, eml,
+        from: "main"
+    }
+    console.log(data)
+    axios.post('sendForm.php', {
+        name: fioname.value,
+        phone: phone.value,
+        email: email.value,
+        nkoname: nkoname.value,
+        from: "nko"
+
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+let form = document.getElementById("myForm");
+form.addEventListener('submit', getConsultation);
 let ansq = "После обсуждения всех деталей будущего сотрудничества – заключаем официальный договор с Некоммерческой Организацией" +
     "Затем вводим куратора от НКО в нюансы работы, предоставляем рабочее место (онлайн) и обучаем его работе в нашем приложении Diagnost" +
     `Куратор всегда имеет доступ к онлайн платформе: там он в реальном времени получает все данные по пациентам и общается с командой ВН`
