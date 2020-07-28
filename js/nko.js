3b5d4ba7d5c6d5d44f3d6d0910a40bd206d6a9e2
Nuruddin <sg7720@gmail.com>
2020-07-28 18:45:52 +0300

    actions in main page and nko page

--- js/nko.js
+++ js/nko.js
@@ -25,14 +25,19 @@ var mySwiper = new Swiper('.swiper-container', {
 })
 let fioname = document.getElementById("fioform")
 let nkoname = document.getElementById("nko-name-form")
-let phone = document.getElementById("phone-form")
-let email = document.getElementById("email-form")
+let nkophone = document.getElementById("phone-form")
+let nkoemail = document.getElementById("email-form")
+let anyname = document.getElementById("name")
+let phone = document.getElementById("phone")
+let email = document.getElementById("eml")
+let anyagree = document.getElementById('agree')
+let nkowarning = document.getElementById("warning")
 let agree = document.getElementById('agr-check')
 let button = document.getElementById("send-button")
 let warning = document.getElementById("warning")
 const getConsultation = (event) => {
     event.preventDefault();
-    if (fioname.value === "" || phone.value === "" || email.value === "") return
+    if (fioname.value === "" || nkophone.value === "" || nkophone.value === "") return
     if (!agree.checked) {
         warning.style.display = "block"
         setTimeout(() => { warning.style.display = "none"; }, 1000)
@@ -45,8 +50,8 @@ const getConsultation = (event) => {
     console.log(data)
     axios.post('sendForm.php', {
         name: fioname.value,
-        phone: phone.value,
-        email: email.value,
+        phone: nkophone.value,
+        email: nkoemail.value,
         nkoname: nkoname.value,
         from: "nko"
 
@@ -61,6 +66,37 @@ const getConsultation = (event) => {
 }
 let form = document.getElementById("myForm");
 form.addEventListener('submit', getConsultation);
+const sendQuestion = (event) => {
+    debugger
+    event.preventDefault();
+    if (anyname.value === "" || phone.value === "" || email.value === "") return
+    if (!anyagree.checked) {
+        warning.style.display = "block"
+        setTimeout(() => { warning.style.display = "none"; }, 1000)
+        return
+    }
+    let data = {
+        name, phone, eml,
+        from: "main"
+    }
+    console.log(data)
+    axios.post('sendForm.php', {
+        name: anyname.value,
+        phone: phone.value,
+        email: email.value,
+        from: "nko-any-questions-form"
+
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+let anyquestionform = document.getElementById("anyquestionsForm");
+anyquestionform.addEventListener('submit', sendQuestion);
 let ansq = "После обсуждения всех деталей будущего сотрудничества – заключаем официальный договор с Некоммерческой Организацией" +
     "Затем вводим куратора от НКО в нюансы работы, предоставляем рабочее место (онлайн) и обучаем его работе в нашем приложении Diagnost" +
     `Куратор всегда имеет доступ к онлайн платформе: там он в реальном времени получает все данные по пациентам и общается с командой ВН`
